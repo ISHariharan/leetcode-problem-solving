d13f122a175e82604ee36d241cb63706971ee12c
Hariharan.I.S <112647530+ISHariharan@users.noreply.github.com>
2026-01-26 21:26:23 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (14.67%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,17 @@
+import math;
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+
+        while(low <= high) :
+            mid = math.floor((low + high) / 2)
+            if (nums[mid] == target):
+                return mid
+            elif (nums[mid] < target):
+                low = mid + 1
+            else:
+                high = mid - 1;
+        return - 1;
+        
