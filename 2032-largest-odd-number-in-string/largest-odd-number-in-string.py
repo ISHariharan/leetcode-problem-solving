3a9b84fdb4570e5f155172eadd1d67e1588e43e6
Hariharan.I.S <112647530+ISHariharan@users.noreply.github.com>
2026-04-13 23:27:59 +0530

    Time: 6 ms (66.06%) | Memory: 20.2 MB (70.33%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
@@ -1,11 +1,6 @@
 class Solution:
-    def largestOddNumber(self, s: str) -> str:
-        pointer = 0
-        largest = ""
-        while pointer < len(s):
-            if int(s[pointer]) % 2 == 1:
-                largest = s[:pointer + 1]
-            pointer += 1
-        if largest == 0:
-            return ""
-        return largest
+    def largestOddNumber(self, num: str) -> str:
+        for i in range(len(num)-1, -1, -1):
+            if int(num[i]) & 1:       
+                return num[:i+1]
+        return ""
