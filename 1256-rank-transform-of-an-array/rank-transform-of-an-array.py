2137e77e9ee600f8d960905b9ef1394c2d5d829e
Hariharan.I.S <112647530+ISHariharan@users.noreply.github.com>
2026-07-12 16:20:21 +0530

    Time: 35 ms (76.35%) | Memory: 37.6 MB (39.04%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
@@ -1,13 +1,12 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> any:
-        sortedArr = sorted(arr)
+        sortedArr = sorted(set(arr))
         mapping = {}
         length = len(arr)
         rank = 1
-        for index in range(length):
-            mapping[sortedArr[index]] = rank
-            if not (index + 1 != length and sortedArr[index] == sortedArr[index + 1]):
-                rank += 1
+        for items in sortedArr:
+            mapping[items] = rank
+            rank += 1
         result = []
         for index in range(length):
             result.append(mapping[arr[index]])
