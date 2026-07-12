3755f5a87a104669c0f2bcec86d8b37627037966
Hariharan.I.S <112647530+ISHariharan@users.noreply.github.com>
2026-07-12 19:15:00 +0530

    Time: 0 ms (100.00%) | Memory: 20 MB (30.26%) - LeetSync

--- 566-reshape-the-matrix/reshape-the-matrix.py
+++ 566-reshape-the-matrix/reshape-the-matrix.py
@@ -5,12 +5,11 @@ class Solution:
             for item in row:
                 flattenArr.append(item)
         length = len(flattenArr)
-        if r * c > length or r * c < length:
-            return mat
-        index = 0
-        mat = [[0] * c for index in range(r)]
-        for row in range(r):
-            for col in range(c):
-                mat[row][col] = flattenArr[index]
-                index += 1
+        if r * c == length : 
+            index = 0
+            mat = [[0] * c for index in range(r)]
+            for row in range(r):
+                for col in range(c):
+                    mat[row][col] = flattenArr[index]
+                    index += 1
         return mat
