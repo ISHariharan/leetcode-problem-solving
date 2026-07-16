27a64be9c0d74bd05c71d286f6ac594fbf225147
Hariharan.I.S <112647530+ISHariharan@users.noreply.github.com>
2026-07-16 06:22:41 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (53.12%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.py
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.py
@@ -1,9 +1,9 @@
 class Solution:
+    def gcd(x, y) -> int:
+        if x == 0:
+            return y
+        return gcd(y, x%y)
     def gcdOfOddEvenSums(self, n: int) -> int:
         sumOfEvenNumbers = n * (n+1)
         sumOfOddNumbers = n ** 2
-        maxiGCD = 0
-        for divisor in range(min(sumOfEvenNumbers, sumOfOddNumbers), -1, -1):
-            if sumOfEvenNumbers % divisor == 0 and sumOfOddNumbers % divisor == 0:
-                return divisor
-        return maxiGCD
+        return gcd(sumOfEvenNumbers, sumOfOddNumbers)
