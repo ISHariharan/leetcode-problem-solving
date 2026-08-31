38d1cbf95f094cd7d2ef9046c8b5ddb5783938cf
Hariharan.I.S <112647530+ISHariharan@users.noreply.github.com>
2026-08-31 23:17:16 +0530

    Time: 93 ms (33.25%) | Memory: 63 MB (77.08%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -7,8 +7,6 @@ class Solution:
     def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
         if head == None or head.next == None or head.next.next == None:
             return [-1, -1]
-        # temp = head
-        # arr = []
         left = head
         mid = head.next
         right = head.next.next
@@ -23,26 +21,16 @@ class Solution:
             mid = mid.next
             right = right.next
             index += 1
-        # left = 0
-        # right = 2
-        # while right < len(arr):
-        #     mid = (left + right) // 2
-        #     if arr[left] < arr[mid] and arr[right] < arr[mid]:
-        #         indexes.append(mid)
-        #     elif arr[left] > arr[mid] and arr[right] > arr[mid]:
-        #         indexes.append(mid)
-        #     left += 1
-        #     right += 1
         if indexes == [] or len(indexes) == 1:
             return [-1, -1]
         indexes = sorted(indexes)
-        maxDistance = indexes[len(indexes) - 1] - indexes[0]
+        length = len(indexes)
+        maxDistance = indexes[length - 1] - indexes[0]
         minDistance = float('inf')
         left = 0
         right = 1
-        while right < len(indexes):
+        while right < length:
             minDistance = min(abs(indexes[right] - indexes[left]) , minDistance)
             left += 1
             right += 1
         return [minDistance, maxDistance]
-        # return indexes
